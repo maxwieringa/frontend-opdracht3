fe6c9b3d14c8206e7275e1573805f484f5a72281
maxwieringa <maxwieringa01@gmail.com>
2020-06-18 13:54:08 +0200

    Aanpassingen

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const section = document.getElementById('joke')
 const upvote = document.getElementById('upvote');
 const downvote = document.getElementById('downvote');
 const newJoke = document.querySelector('button');
+const upvoteImg = document.getElementById('upvoteimg');
+const downvoteImg = document.getElementById('downvoteimg');
 
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
@@ -21,6 +23,7 @@ xhr.setRequestHeader("x-rapidapi-key", "a8b6dd0768msh1e6f6d88e2dff38p19821cjsn1e
 xhr.responseType = 'json';
 xhr.send(data);
 
+// Functies die worden uitgevoerd wanneer de pagina laadt.
 xhr.onload = function() {
   const jokes = xhr.response;
   jokeParagraph(jokes);
@@ -28,31 +31,69 @@ xhr.onload = function() {
   populateDownvotes(jokes);
 }
 
+// Maakt paragraaf element waarin de grap wordt gedaan die door de json verstuurd wordt
 function jokeParagraph(jsonObj) {
   const myPara = document.createElement('p');
   myPara.textContent = jsonObj['content'];
   section.appendChild(myPara);
 }
 
+// Maakt een paragraaf aan zodat je het aantal upvotes ziet
 function populateUpvotes(jsonObj) {
   const upvotePara = document.createElement('p');
+  upvotePara.setAttribute('id', 'upvotePara');
   upvotePara.textContent = jsonObj['upvotes'];
   upvote.appendChild(upvotePara);
+  return;
 }
 
+// Maakt een paragraaf aan zodat je het aantal downvotes ziet
 function populateDownvotes(jsonObj) {
   const downvotePara = document.createElement('p');
+  downvotePara.setAttribute('id', 'downvotePara');
   downvotePara.textContent = jsonObj['downvotes'];
   downvote.appendChild(downvotePara);
+  return;
 }
 
+// Reload de pagina
 function reload() {
   window.location.reload(true);
 }
 
+// Doet +1 op de huidge aantal upvotes
+function upvoteAdd() {
+  const upvotePara = document.getElementById('upvotePara');
+  const downvotePara = document.getElementById('downvotePara');
+  let upvoteTxt = upvotePara.textContent;
+  let upvoteNumber = parseFloat(upvoteTxt);
+  let downvoteTxt = downvotePara.textContent;
+  let downvoteNumber = parseFloat(downvoteTxt);
+  upvotePara.textContent = upvoteNumber + 1;
+  downvotePara.textContent = downvoteNumber - 1;
+  upvoteImg.removeEventListener('click', upvoteAdd);
+}
+
+// Doet +1 op de huidige aantal downvotes
+function downvoteAdd() {
+  const downvotePara = document.getElementById('downvotePara');
+  const upvotePara = document.getElementById('upvotePara');
+  let downvoteTxt = downvotePara.textContent;
+  let downvoteNumber = parseFloat(downvoteTxt);
+  let upvoteTxt = upvotePara.textContent;
+  let upvoteNumber = parseFloat(upvoteTxt);
+  downvotePara.textContent = downvoteNumber + 1;
+  upvotePara.textContent = upvoteNumber - 1;
+  downvoteImg.removeEventListener('click', downvoteAdd);
+}
+
+// Alle eventlisteners
 newJoke.addEventListener('click', reload);
 window.addEventListener("keydown", function(event) {
   if (event.keyCode == 32) {
     reload();
   }
 });
+
+upvoteImg.addEventListener('click', upvoteAdd);
+downvoteImg.addEventListener('click', downvoteAdd);
